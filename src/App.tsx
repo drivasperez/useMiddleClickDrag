58237c6d8b16c118cb573adb7d1fed5fbfc3c0e2
Daniel Rivas <daniel@silicoai.com>
2022-09-16 17:05:45 +0100

    Update deps and build demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,97 @@
 import "./styles.css";
 import React from "react";
 
-function isMiddleClick(ev: MouseEvent) {
-  return ev.button === 1;
+interface ScrollState {
+  left: number;
+  top: number;
+  x: number;
+  y: number;
 }
 
 function useMiddleClickDrag() {
   const [ref, setRef] = React.useState<null | HTMLElement>(null);
+  const [isDragging, setIsDragging] = React.useState(false);
+  const posRef = React.useRef<ScrollState>({ top: 0, left: 0, x: 0, y: 0 });
 
   React.useEffect(() => {
-    const mouseDownListener = (ev: MouseEvent) => {
-      console.log("Mouse event:", ev.button);
-      if (isMiddleClick(ev)) {
-        ev.preventDefault();
-      }
-    };
-
     if (ref) {
+      // When the user middle clicks, record current scroll position and track
+      // mousemove and mouseup events on window.
+      const mouseDownListener = (ev: MouseEvent) => {
+        if (isMiddleClick(ev)) {
+          setIsDragging(true);
+          posRef.current = {
+            left: ref.scrollLeft,
+            top: ref.scrollTop,
+            x: ev.clientX,
+            y: ev.clientY,
+          };
+          window.addEventListener("mousemove", mouseMoveListener);
+          // document.addEventListener("mouseenter", mouseEnterListener);
+          window.addEventListener("mouseup", mouseUpListener);
+        }
+      };
+
+      // If the middle mouse button is held, scroll the container on mousemove.
+      const mouseMoveListener = (ev: MouseEvent) => {
+        const { clientX, clientY } = ev;
+        const dx = clientX - posRef.current.x;
+        const dy = clientY - posRef.current.y;
+
+        ref.scrollLeft = posRef.current.left - dx;
+        ref.scrollTop = posRef.current.top - dy;
+      };
+
+      // If the mouse enters the window and middle button is not held, that means
+      // the user stopped middle clicking outside of the window, which the mouseUpListener
+      // will not catch. So stop the dragging.
+      const mouseEnterListener = (ev: MouseEvent) => {
+        if (!hasMiddleClickHeld(ev)) {
+          setIsDragging(false);
+          window.removeEventListener("mousemove", mouseMoveListener);
+          window.removeEventListener("mouseup", mouseUpListener);
+        }
+      };
+
+      // If the user stops holding the middle mouse button, remove the event listeners.
+      const mouseUpListener = (ev: MouseEvent) => {
+        if (isMiddleClick(ev)) {
+          setIsDragging(false);
+          window.removeEventListener("mouseup", mouseUpListener);
+          window.removeEventListener("mousemove", mouseMoveListener);
+          window.removeEventListener("mouseenter", mouseEnterListener);
+        }
+      };
+
       ref.addEventListener("mousedown", mouseDownListener);
       return () => {
+        // Remove all the event listeners just in case.
         ref.removeEventListener("mousedown", mouseDownListener);
+        window.removeEventListener("mousemove", mouseMoveListener);
+        window.removeEventListener("mouseenter", mouseEnterListener);
+        window.removeEventListener("mouseup", mouseUpListener);
       };
     }
   }, [ref]);
 
-  return [setRef];
+  return [setRef, isDragging] as const;
 }
 
 export default function App() {
-  const [ref] = useMiddleClickDrag();
+  const [ref, isDragging] = useMiddleClickDrag();
 
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{ cursor: isDragging ? "all-scroll" : "default" }}
+    >
       <div
         ref={ref}
         style={{
           overflow: "auto",
           border: "1px solid green",
           width: "400px",
-          height: "400px"
+          height: "400px",
         }}
       >
         <div
@@ -47,10 +100,18 @@ export default function App() {
             width: "5000px",
             background:
               "url(https://www.google.com/logos/doodles/2022/hm-queen-elizabeth-ii-6753651837109804.2-s.png)",
-            backgroundRepeat: "repeat"
+            backgroundRepeat: "repeat",
           }}
         ></div>
       </div>
     </div>
   );
+}
+
+function isMiddleClick(ev: MouseEvent) {
+  return ev.button === 1;
+}
+
+function hasMiddleClickHeld(ev: MouseEvent) {
+  return ev.buttons === 4;
 }
